53f6500c9e30f438e6da28bf817ebc13dd378623
Anna Kozachenko <annakozacenko@yandex.ru>
2025-04-13 00:19:30 +0300

    создана минимальная структура

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,5 +1,20 @@
 import styles from './app-header.module.css';
 import clsx from 'clsx';
 export const AppHeader = () => {
-    return <h1>AppHeader</h1>
+    return (
+        <nav className={styles.menu}>
+        <div className="menu-1">
+          <img className="home-work-figr-icon" src="src/assets/add_Figr_Icon.svg" />
+          <div className="nav-title2">Home</div>
+        </div>
+        <div className="menu-2">
+          <img className="home-work-figr-icon2" src="src/assets/add_Figr_Icon.svg" />
+          <div className="nav-title2">Favorites</div>
+        </div>
+        <div className="menu-3">
+          <img className="home-work-figr-icon3" src="src/assets/add_Figr_Icon.svg" />
+          <div className="nav-title3">Profile</div>
+        </div>
+        </nav>
+    )
 }
